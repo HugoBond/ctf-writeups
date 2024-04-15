450e898584d1b2573974c730d3a4ed7f345ef016
Hugo <72299419+HugoBond@users.noreply.github.com>
2024-04-15 16:21:32 +0200

    New chall and trash removed

--- B01lers CTF/shamir for dummies/solve.py
+++ B01lers CTF/shamir for dummies/solve.py
@@ -1,23 +1,35 @@
 from pwn import remote
-from Crypto.Util.number import getPrime,long_to_bytes
+from Crypto.Util.number import long_to_bytes,GCD
+from sage.all import PolynomialRing, GF
+
 server = remote("gold.b01le.rs", 5006)
 server.recvuntil("Let's use \n\n")
 n = int(server.recvline(b'n =').strip().split(b"=")[1])
 k = int(server.recvline(b'k =').strip().split(b"=")[1])
 p = int(server.recvline(b'p =').strip().split(b"=")[1])
-print(f"n = {n}, k = {k}, p = {p}")
-count = 0
-#prime = getPrime(490)
+print(f"n = {n}\nk = {k}\np = {p}")
 
+def roots_of_unity(e, phi, n):    
+    phi_coprime = phi
+    while GCD(phi_coprime, e) != 1:
+        phi_coprime //=  GCD(phi_coprime, e)
+
+    roots = pow(2, phi_coprime, n) 
+    return roots
+
+
+root= roots_of_unity(n,p-1, p)
+print(f"Root of unity: {root}")
+count = 0
 while count < n:
-    if count == 0:
-        server.sendlineafter(b'> ',str(p-(p-54)).encode())
-    elif count == 1:
-        server.sendlineafter(b'> ',str(p-54).encode())
-    else:
-        server.sendlineafter(b'> ',str(count).encode())
+    server.sendlineafter(b'> ',str((root**count) % p).encode())
     count += 1
 
 server.recvuntil(b'> ')
-server.sendline(str(pow(n,-1,p)).encode())
-server.interactive()
+server.sendline(str(n).encode())
+server.recvuntil(b"The shares P(X_i)'s were':\n")
+shares = list(map(int,server.recvline().strip().decode()[1:-1].split(", ")))
+P = PolynomialRing(GF(p), 'x')
+res = P.lagrange_polynomial(list((root**i % p,shares[i]) for i in range(n)))
+coefficients = res.coefficients()
+print(f"Flag: {long_to_bytes(int(coefficients[0]))}")
